150a3133bbed043783088b193bc84a3b96de33c0
Bhavneesh Sharma <54917282+bs3537@users.noreply.github.com>
2020-03-04 12:36:20 -0500

    Add files via upload

---
+++ api/api2.py
@@ -0,0 +1,23 @@
+#https://www.kdnuggets.com/2019/01/build-api-machine-learning-model-using-flask.html
+
+from flask import Flask, request, redirect, url_for, flash, jsonify
+import pickle 
+import joblib
+import json
+import numpy as np
+
+def create_app():
+    app = Flask(__name__)
+
+
+    @app.route('/api/', methods=['POST'])
+    def prediction():
+        data = request.get_json()
+        prediction = np.array2string(model.predict(data))
+
+        return jsonify(prediction)
+    return app
+
+if __name__ == '__main__':
+    model = joblib.load('https://storage.cloud.google.com/medcabapi/baseline_model2.pkl')
+    app.run(debug=True)
